31f77f30630764152354fc51f9e63dbd07d0902f
Asher Glick <asher.glick@gmail.com>
2012-02-02 09:49:22 -0500

    ran some tests regarding ghost players on other world

--- NonPlayers/src/iggy/NonPlayers/NonPlayers.java
+++ NonPlayers/src/iggy/NonPlayers/NonPlayers.java
@@ -38,10 +38,9 @@ public class NonPlayers extends JavaPlugin{
 		pluginTitle = "[\033[0;43m"+pluginName+"\033[0m]";
 		this.logger.info(pluginTitle+ " version " + pdFile.getVersion() +" is enabled");
 		// TODO Auto-generated method stub
-		Location point = new Location (this.getServer().getWorld("world"),0,64,0);
+		Location point = new Location (this.getServer().getWorld("world"),0,1,0);
 		Player iggy = (Player) manager.spawnHumanNPC("Iggystlev", point).getBukkitEntity();
 		iggy.setItemInHand(new ItemStack(Material.DIAMOND_SWORD));
-		iggy.setDisplayName("igy");
 		String players[] = {"A Guard","Guard", "Notch", "God_Demeter", "Grandma","Smith","jeb","Killer","God","kroden3d","klingbolt","IbramGaunt00","Owner","Shopkeeper","Top","Guard","Ninja","hidden","LeaverBoy","metalheadcz","Icarus","Dominus","Miner","Goblin","Citizen","Maokai","Judge", "Guard","Herobrine","PigZombie","KingCharles","Oracle","Librarian","augphlosiom","Basic","Wolfram","EnlightendDead","Hidendra","Drakia", "Quaffle","Jereq","HoneyDew","Xephos","Israphel","Grim","Ezreal","Jason","Non","Gazz","Britannia","Little","aPunch","Chris","Joe","Engel","Blockmann","Chef","Archer","CaptainSparklez","Kaikez","Adventurer","Warami","SoldjahBoy","frosty","Joymo","AnewAaron","TheDarkStar","Geecku","Shoigun","Giik","seekercat","Yamatsukami","MakaHearts","Kraiten","WingOfWar","ThePengu","Raecchi","Amber","MizLiv","AngelGirl737","SuccubusQueen"};
 		for (int i = 0; i < players.length; i++){
 			Location pos = new Location(this.getServer().getWorld("world"),2*(i+1),64,0);
@@ -53,6 +52,14 @@ public class NonPlayers extends JavaPlugin{
 			
 		}
 		
+	}
+	
+	// helpfull functions for output
+	public void info(String input) {
+		this.logger.info(pluginTitle + input);
+	}
+	public void severe (String input) {
+		this.logger.severe(pluginTitle+"\033[31m"+input+"\033[0m");
 	}
 
 }
