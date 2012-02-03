566108bc051e8daf2f184e79f8d5994e8bdb62a0
Asher Glick <asher.glick@gmail.com>
2012-02-03 18:24:46 -0500

    Region names are displayed when you enter a new area

---
+++ Regions/src/iggy/Regions/DisplayPlotTitles.java
@@ -0,0 +1,92 @@
+/******************************************************************************\
+|                                     ,,                                       |
+|                    db             `7MM                                       |
+|                   ;MM:              MM                                       |
+|                  ,V^MM.    ,pP"Ybd  MMpMMMb.  .gP"Ya `7Mb,od8                |
+|                 ,M  `MM    8I   `"  MM    MM ,M'   Yb  MM' "'                |
+|                 AbmmmqMA   `YMMMa.  MM    MM 8M""""""  MM                    |
+|                A'     VML  L.   I8  MM    MM YM.    ,  MM                    |
+|              .AMA.   .AMMA.M9mmmP'.JMML  JMML.`Mbmmd'.JMML.                  |
+|                                                                              |
+|                                                                              |
+|                                ,,    ,,                                      |
+|                     .g8"""bgd `7MM    db        `7MM                         |
+|                   .dP'     `M   MM                MM                         |
+|                   dM'       `   MM  `7MM  ,p6"bo  MM  ,MP'                   |
+|                   MM            MM    MM 6M'  OO  MM ;Y                      |
+|                   MM.    `7MMF' MM    MM 8M       MM;Mm                      |
+|                   `Mb.     MM   MM    MM YM.    , MM `Mb.                    |
+|                     `"bmmmdPY .JMML..JMML.YMbmd'.JMML. YA.                   |
+|                                                                              |
+\******************************************************************************/
+/******************************************************************************\
+| Copyright (c) 2012, Asher Glick                                              |
+| All rights reserved.                                                         |
+|                                                                              |
+| Redistribution and use in source and binary forms, with or without           |
+| modification, are permitted provided that the following conditions are met:  |
+|                                                                              |
+| * Redistributions of source code must retain the above copyright notice,     |
+|   this list of conditions and the following disclaimer.                      |
+| * Redistributions in binary form must reproduce the above copyright notice,  |
+|   this list of conditions and the following disclaimer in the documentation  |
+|   and/or other materials provided with the distribution.                     |
+|                                                                              |
+| THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  |
+| AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE    |
+| IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE   |
+| ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE    |
+| LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR          |
+| CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF         |
+| SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS     |
+| INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN      |
+| CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)      |
+| ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE   |
+| POSSIBILITY OF SUCH DAMAGE.                                                  |
+\******************************************************************************/
+
+package iggy.Regions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+
+/***************************** DISPLAY PLOT TITLES ****************************\
+| This class is made so that when a user enters or leaves a plot the name is   |
+| displayed in their chat bar. Pass in the plugin as an argument to the        |
+| constructor. To activate the listener run the EnableRegionDispayNames        |
+| function and it will automatically grab the names                            |
+\******************************************************************************/
+
+public class DisplayPlotTitles implements Listener{
+	Map<Player,String> lastLocation = new HashMap<Player,String>();
+	Regions plugin;
+	
+	DisplayPlotTitles (Regions instance){
+		plugin = instance;
+	}
+	
+	public void EnableRegionDisplayNames() {
+		Bukkit.getPluginManager().registerEvents(this, plugin);
+	}
+	
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void playerWalk (PlayerMoveEvent event){
+		String plotName = plugin.chunkNames.get(new Position(event.getTo()));
+		String playerSpot = lastLocation.get(event.getPlayer());
+		if (plotName == null){plotName = "The Wild";}
+		if (playerSpot == null){playerSpot = "The Wild";}
+		if (plotName != playerSpot){
+			lastLocation.put(event.getPlayer(), plotName);
+			event.getPlayer().sendMessage("You have entered :"+ChatColor.LIGHT_PURPLE +plotName+ ChatColor.WHITE);
+		}
+	}
+}
