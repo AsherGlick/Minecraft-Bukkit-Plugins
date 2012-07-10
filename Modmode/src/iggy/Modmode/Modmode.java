fe1a8c22448a898ac77950661a6b249190a6e4d8
Asher Glick <asher.glick@gmail.com>
2012-07-10 15:20:30 -0400

    upgraded the code for modmode, it will now have its place on the server

--- Modmode/src/iggy/Modmode/Modmode.java
+++ Modmode/src/iggy/Modmode/Modmode.java
@@ -3,6 +3,7 @@ package iggy.Modmode;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.logging.Logger;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -11,16 +12,35 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 //import org.bukkit.potion.PotionEffect;
 //import org.bukkit.potion.PotionEffectType;
 
 
 public class Modmode extends JavaPlugin{
+	Logger logger = Logger.getLogger("Minecraft");
+	
+	String pluginTitle;
+	PluginDescriptionFile pdFile;
+	
 	public Map<Player, Double> lightningDegree = new HashMap<Player, Double>();
 	public double distance = 7;
+  //////////////////////////////////////////////////////////////////////////////
+ ////////////////////////////// ENABLE / DISABLE //////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+	@Override
+	public void onDisable() {
+		info(" Version " + pdFile.getVersion() +" is disabled");
+	}
+	
 	@Override
 	public void onEnable () {
+		pdFile = this.getDescription();
+		String pluginName = pdFile.getName();
+		pluginTitle = "[\033[2;35m"+pluginName+"\033[0m]";
+		
+		
 		getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable () {
 			public void run() {
 				for (Entry<Player,Double> p : lightningDegree.entrySet()) {
@@ -42,7 +62,11 @@ public class Modmode extends JavaPlugin{
 				}
 			}
 		}, 2L, 2L);
+		
+		info (" Version " + pdFile.getVersion() +" is enabled");
 	}
+	
+
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		Player player = null;
@@ -51,8 +75,12 @@ public class Modmode extends JavaPlugin{
 		}
 		
 		if (player == null) {
-			//this.logger.info("This command can only be run by a player");
+			info("This command can only be run by a player");
 			return false;
+		}
+		
+		if (!player.isOp()) {
+			player.sendMessage("Stop trying to cheat!");
 		}
 		
 		//World world = player.getWorld();
@@ -67,9 +95,9 @@ public class Modmode extends JavaPlugin{
 				lightningDegree.put(player, 0D);
 				if (player.getItemInHand().getType() == Material.DIAMOND_SWORD) {
 					player.getItemInHand().addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 999);
+					player.getItemInHand().addUnsafeEnchantment(Enchantment.DURABILITY, 999);
+					player.getItemInHand().setType(Material.WOOD_SWORD);
 				}
-				//player.addPotionEffect(new PotionEffect(PotionEffectType., 0, 0));
-				//PotionEffect effect = new PotionEffect(null, 0, 0);
 			}
 			
 		}
@@ -86,5 +114,25 @@ public class Modmode extends JavaPlugin{
 			}
 		}
 		return false;
+	}
+  //////////////////////////////////////////////////////////////////////////////
+ /////////////////////////////// DISPLAY HELPERS //////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+	/********************************** LOG INFO **********************************\
+	| The log info function is a simple function to display info to the console    |
+	| logger. It also prepends the plugin title (with color) to the message so     |
+	| that the plugin that sent the message can easily be identified               |
+	\******************************************************************************/
+	public void info(String input) {
+		this.logger.info(pluginTitle + input);
+	}
+	
+	/********************************* LOG SEVERE *********************************\
+	| The log severe function is very similar to the log info function in that it  |
+	| displays information to the console, but the severe function sends a SEVERE  |
+	| message instead of an INFO. It also turns the message text red               |
+	\******************************************************************************/
+	public void severe (String input) {
+		this.logger.severe(pluginTitle+"\033[31m"+input+"\033[0m");
 	}
 }
